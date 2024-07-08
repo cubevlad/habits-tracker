4f24448e496326a617e92c252784f5926f498176
cubevlad <kubasovvv07@gmail.com>
2024-07-08 22:43:28 +0400

    fix: lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,14 +1,20 @@
-const eslintRules = 'cd client && eslint --max-warnings=0'
+const eslintRules = 'eslint --max-warnings=0'
+const prettierRules = 'prettier --ignore-unknown --write'
+const stylelintRules = 'stylelint --max-warnings=0 --config ./.stylelintrc'
+const vitestRules = 'vitest run'
 
-const prettierRules = 'cd client && prettier --ignore-unknown --write'
+const clientConfig = {
+  './client/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}': [vitestRules],
+  './client/*.{ts,tsx,js,jsx}': [eslintRules, stylelintRules, prettierRules],
+  './client/*.{css,scss}': [stylelintRules, prettierRules],
+  './client/!*.{js,jsx,ts,tsx,css,scss}': [prettierRules],
+}
 
-const stylelintRules = 'cd client && stylelint --max-warnings=0 --config ./.stylelintrc'
+const serverConfig = {
 
-const vitestRules = 'cd client && vitest run'
+}
 
 export default {
-  '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}': [vitestRules],
-  '*.{ts,tsx,js,jsx}': [eslintRules, stylelintRules, prettierRules],
-  '*.{css,scss}': [stylelintRules, prettierRules],
-  '!*.{js,jsx,ts,tsx,css,scss}': [prettierRules],
+  ...clientConfig,
+  ...serverConfig
 }
