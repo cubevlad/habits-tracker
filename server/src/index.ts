bc05267afd44c4a8e8c8b401c8c00099d026a459
cubevlad <kubasovvv07@gmail.com>
2024-07-09 09:34:48 +0400

    added tsconfig for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,14 @@
 import http from 'node:http'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const server = http.createServer((request, response) => {
-  // magic happens here!
+const HOST = '127.0.0.1'
+const PORT = 3000
+
+const server = http.createServer((_request, response) => {
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'text/plain')
+  response.end('Hello World')
+})
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`)
 })
